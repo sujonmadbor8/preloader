d763c36be29146d15355f1023c3d4294dddf6e2b
Sujon Madbor <sujonmadbor8@gmail.com>
2023-05-03 10:52:58 +0600

    added

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// javaScript code here
+
+// jQuery code here
+$(function () {
+  // preloader start
+  $(window).load(function () {
+    $("#loading").fadeOut(400);
+  });
+  // preloader end
+});
